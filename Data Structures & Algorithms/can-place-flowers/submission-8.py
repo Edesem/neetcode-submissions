06f3f94c0b02787e55730b75ae59d27132ffc66e
Mashed <93417266+Edesem@users.noreply.github.com>
2026-05-30 10:13:03 +1000

    Add: can-place-flowers - submission-8

---
+++ Data Structures & Algorithms/can-place-flowers/submission-8.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        res = 0
+        counter = 0
+        length = len(flowerbed) - 1
+
+        for i, flower in enumerate(flowerbed):
+            # 3 tile
+            if flower == 0:
+                counter += 1
+            else:
+                counter = 0
+
+            if length + 1 == 1 and counter == 1:
+                res += 1
+                
+            elif counter == 2:
+                if i < length and flowerbed[i + 1] == 0:
+                    flowerbed[i] = 1
+                    counter = 0
+                    res += 1
+                elif i == 1 and flowerbed[i - 1] == 0:
+                    flowerbed[i - 1] = 1
+                    counter = 0
+                    res += 1
+                elif i == length and flowerbed[i - 1] == 0:
+                    flowerbed[i] = 1
+                    counter = 0
+                    res += 1
+
+
+            print(i, flower, counter, res)
+            
+
+           # print(i, flower, counter)
+
+        print(flowerbed)
+        print(res, n)
+        return res >= n
